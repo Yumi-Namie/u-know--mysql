e51612936d2f3abf409609201b93f18766250e77
huilenpe@gmail.com <131238839+HuilenPe@users.noreply.github.com>
2023-06-04 01:17:34 +0200

    Primeras consultas

--- mysql/uknow-consult-file.sql
+++ mysql/uknow-consult-file.sql
@@ -1,2 +1,16 @@
 USE db_uknow;
 
+/* Consultar contenidos ordenados por precio descendiente*/
+SELECT * FROM contents ORDER BY current_price DESC
+
+/* Consultar todos los contenidos ordenados por fecha de creación descendente */
+SELECT * FROM contents ORDER BY update_date DESC;
+
+/* Consultar todos los contenidos ordenados por dificultad */
+SELECT * FROM contents ORDER BY difficult_level DESC;
+
+/* Consultar todos los contenidos ordenados por valoración */
+SELECT * FROM ratings ORDER BY rating DESC;
+
+/* Consultar el saldo de un usuario registrado */
+SELECT balance FROM users WHERE user_name = "nombre del usuario";
